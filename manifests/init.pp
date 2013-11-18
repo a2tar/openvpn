e96373cbe9fb76c850968f474cf54314c5e9e039
a2tar <a2@stealthq.com>
2013-11-19 01:08:24 +0200

    initial commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,3 @@
-# Class: openvpn
-#
-# This module manages openvpn
-#
-# Parameters: none
-#
-# Actions:
-#
-# Requires: see Modulefile
-#
-# Sample Usage:
-#
 class openvpn {
-
+	include openvpn::install
 }
